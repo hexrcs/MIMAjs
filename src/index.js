f548bbaabcbecc15ce1eda42e42ba4c748341e00
Xiaoru Li <thexrcs@gmail.com>
2017-10-02 15:24:14 +0200

    Import BitFriendly and initialize "registers"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = {
+// import libraries
+const bin = require("bitfriendly").bitArray;
+const op = require("bitfriendly").operate;
 
+function empty24() {
+  return new Array(24).fill(false);
 }
+
+function empty20() {
+  return new Array(20).fill(false);
+}
+
+// constant register with value of 1 in 24-bit binary
+let one = new Array(23).fill(false);
+one.push(true);
+
+// 24-bit registers
+let [accu, regX, regY, regZ, regIns, regStr] =
+  [empty24(), empty24(), empty24(), empty24(), empty24(), empty24()];
+
+// 20-bit registers
+let [adrIns, adrStr] =
+  [empty20(), empty20()];
+
