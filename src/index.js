194db22f43ecfa1a9ebf0d4f4014bd5b5f213e52
Xiaoru Li <thexrcs@gmail.com>
2017-10-02 16:20:32 +0200

    Implement 3-bit signal controlled ALU operations

--- src/index.js
+++ src/index.js
@@ -22,3 +22,34 @@ let [accu, regX, regY, regZ, regIns, regStr] =
 let [adrIns, adrStr] =
   [empty20(), empty20()];
 
+function ALU(signal = [false, false, false]) {
+  signal = bin(signal); // unify format first
+  if (signal.length !== 3) {
+    throw new Error("Invalid ALU OpCode: " + op(signal).toString());
+  }
+
+  switch (signal.toString()) {
+    case "000":
+      break;
+    case "001":
+      regZ = op(regX).add(regY).result;
+      break;
+    case "010":
+      regZ = op(regX).rcr().result;
+      break;
+    case "011":
+      regZ = op(regX).and(regY).result;
+      break;
+    case "100":
+      regZ = op(regX).or(regY).result;
+      break;
+    case "101":
+      regZ = op(regX).xor(regY).result;
+      break;
+    case "110":
+      regZ = op(regX).not().result;
+      break;
+    case "111":
+      regZ = op(regX).eql(regY).result;
+  }
+}
